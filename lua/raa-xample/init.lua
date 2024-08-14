8662643a67287d6a8b8dd7e95e54aeb89077ac82
raa <alexander.thequeen@gmail.com>
2024-08-14 22:27:19 +0300

    saving

--- lua/raa-xample/init.lua
+++ lua/raa-xample/init.lua
@@ -7,6 +7,15 @@ local function setup(parameters)
 	print('--> SETUPing')
 end
 
+-- add a command
+vim.api.nvim_create_user_command(
+	'RaaCommand',
+	function(input)
+		print "----> inside RaaCommand"
+	end,
+	{bang = true, desc = 'an example raa-command'}
+)
+
 
 local function say_hellow()
 	print('---> hell-0')
