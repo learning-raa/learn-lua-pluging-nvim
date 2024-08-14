4eaae9cdbfa0140d91ca0a897b7701f254ce59e0
raa <alexander.thequeen@gmail.com>
2024-08-14 16:40:36 +0300

    saving

---
+++ lua/raa-xample/init.lua
@@ -0,0 +1,16 @@
+-- init.lua (raa-xample)
+print('--> inside raa-xample..')
+
+local function say_hellow()
+	print('---> hell-0')
+end
+
+local function print_test()
+	print('#### TEST ####')
+end
+
+return {
+	sayHellow = say_hellow,
+	printTest = print_test
+}
+
