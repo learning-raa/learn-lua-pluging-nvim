71a9703c11627d5e1998f48ee9babb500eefd46f
raa <alexander.thequeen@gmail.com>
2024-08-15 01:36:08 +0300

    saving

--- lua/raa-xample/init.lua
+++ lua/raa-xample/init.lua
@@ -1,33 +1,50 @@
--- init.lua (raa-xample)
--- print('--> inside raa-xample..')
+-- simple api-function
+local function say_hellow()
+	print('---> hell-0')
+end
+
+-- something more interesting
+local api = vim.api
+
+local buf
 
 -- convention setup finction
 -- require'module'.setup({ key = "value"})
 local function setup(parameters)
 	print('--> raa-xample.SETUPing')
+	-- create temporary bufffer and name it
+	buf = api.nvim_create_buf(true, true)
+	api.nvim_buf_set_name(buf, '*raa-buf*')
 end
+
+
+local function run_command()
+	-- create floating window connected to 'buf'
+	local win_id = api.nvim_open_win(
+		buf,
+		true,
+		{
+			relative = 'win', col = 10, row = 10, width = 30, height = 5,
+			border = 'rounded',
+			style = 'minimal',
+		}
+	)
+end
+
 
 -- add a command
 vim.api.nvim_create_user_command(
 	'RaaCommand',
 	function(input)
-		print "----> inside RaaCommand"
+		-- print "----> inside RaaCommand"
+		run_command()
 	end,
 	{bang = true, desc = 'an example raa-command'}
 )
 
-
-local function say_hellow()
-	print('---> hell-0')
-end
-
-local function print_test()
-	print('#### TEST ####')
-end
-
+-- export api
 return {
 	setup = setup,
 	sayHellow = say_hellow,
-	printTest = print_test,
 }
 
